a7009a8f118e2b5db43abf641f57bae8b13792ab
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2025-01-27 13:56:00 -0700

    I think werer good with program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,145 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Tracing;
+using System.Linq;
+using mission_4;
 Console.WriteLine("Hello, World!");
 
 //Welcome the user to the game
+Console.WriteLine("Welcome to the game!");
+
+
+// initializing the player 1 and player 2 values
+int player1 = 0;
+int player2 = 0;
 
 //Create a game board array to store the players' choices
+ char[,] playfield =
+{
+    {'1', '2', '3' },
+    {'4', '5', '6' },
+    {'7', '8', '9' }
+};
+
+ int turns = 0;
+
+
+// declaring GameOver Tuple
+(bool isOver, char winner) gameOver = (false, 'x');
+
+bool IsInPlayfield(char[,] playfield, int number)
+{
+    char charNumber = number.ToString()[0]; // Convert number to its char representation
+    for (int i = 0; i < playfield.GetLength(0); i++)
+    {
+        for (int j = 0; j < playfield.GetLength(1); j++)
+        {
+            if (playfield[i, j] == charNumber)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+do
+{
+    Console.WriteLine("Player 1 which space would you like to take?");
+
+    string input = Console.ReadLine();
+
+    if (int.TryParse(input, out player1) && IsInPlayfield(playfield, player1))
+    {
+        Console.WriteLine($"{player1} is a valid number and exists in the playfield.");
+    }
+    else
+    {
+        Console.WriteLine("Invalid input or the number does not exist in the playfield.");
+    }
+    char playerSign1 = 'X';
+    char playerSign2 = 'O';
+
+    switch (player1)
+    {
+        case 1:
+            playfield[0, 0] = playerSign1; break;
+        case 2:
+            playfield[0, 1] = playerSign1; break;
+        case 3:
+            playfield[0, 2] = playerSign1; break;
+        case 4:
+            playfield[1, 0] = playerSign1; break;
+        case 5:
+            playfield[1, 1] = playerSign1; break;
+        case 6:
+            playfield[1, 2] = playerSign1; break;
+        case 7:
+            playfield[2, 0] = playerSign1; break;
+        case 8:
+            playfield[2, 1] = playerSign1; break;
+        case 9:
+            playfield[2, 2] = playerSign1; break;
+
+    }
+
+    turns++;
+    ticTacToe(playfield);
+    gameOver = ticTacToeWinner(playfield, turns);
+
+    Console.WriteLine("Player 2 which space would you like to take?");
+    input = Console.ReadLine();
+
+    if (int.TryParse(input, out player2) && IsInPlayfield(playfield, player2))
+    {
+        Console.WriteLine($"{player2} is a valid number and exists in the playfield.");
+    }
+    else
+    {
+        Console.WriteLine("Invalid input or the number does not exist in the playfield.");
+    }
+    switch (player2)
+    {
+        case 1:
+            playfield[0, 0] = playerSign2; break;
+        case 2:
+            playfield[0, 1] = playerSign2; break;
+        case 3:
+            playfield[0, 2] = playerSign2; break;
+        case 4:
+            playfield[1, 0] = playerSign2; break;
+        case 5:
+            playfield[1, 1] = playerSign2; break;
+        case 6:
+            playfield[1, 2] = playerSign2; break;
+        case 7:
+            playfield[2, 0] = playerSign2; break;
+        case 8:
+            playfield[2, 1] = playerSign2; break;
+        case 9:
+            playfield[2, 2] = playerSign2; break;
+
+    }
+    turns++;
+
+    ticTacToe(playfield);
+    gameOver = ticTacToeWinner(playfield, turns);
+
+    
+} while (!gameOver.isOver);
+
+
+
+if (gameOver.winner == 'X')
+{
+    Console.WriteLine("Player 1 Congratulations you won the game!");
+}
+else if (gameOver.winner == 'O')
+{
+    Console.WriteLine("Player 2 Congratulations you won the game!");
+}
+
+
+
 
 //Ask each player in turn for their choice and update the game board array
 
@@ -12,4 +148,3 @@ Console.WriteLine("Hello, World!");
 /* Check for a winner by calling the method in the supporting class, and notify the players
  When a win has occurred and which player won the game */
 
-Console.WriteLine("Hello Josh")
